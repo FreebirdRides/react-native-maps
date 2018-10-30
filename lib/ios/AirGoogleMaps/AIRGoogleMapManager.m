142bbec185e52553d6563978503f505588acf423
theonetheycallneo <anthony.wentzel@gmail.com>
2018-10-30 14:07:49 -0700

    fix: ios9 crash

--- lib/ios/AirGoogleMaps/AIRGoogleMapManager.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMapManager.m
@@ -436,10 +436,22 @@ RCT_REMAP_METHOD(getDirections,
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
   MKDirectionsRequest *request = [[MKDirectionsRequest alloc] init];
-  MKPlacemark *placemarkOrigin = [[MKPlacemark alloc] initWithCoordinate:origin];
+  MKPlacemark *placemarkOrigin = nil;
+  // NOTE: iOS 10 v iOS 3-9 deprecated function
+  //   https://stackoverflow.com/questions/41486370/crash-on-ios-9-only-mkplacemark-initwithcoordinate-unrecognized-selector
+  if ([MKPlacemark respondsToSelector:@selector(initWithCoordinate:)]) {
+    placemarkOrigin = [[MKPlacemark alloc] initWithCoordinate:origin];
+  } else {
+    placemarkOrigin = [[MKPlacemark alloc] initWithCoordinate:origin addressDictionary:nil];
+  }
   MKMapItem *mapItemOrigin = [[MKMapItem alloc] initWithPlacemark:placemarkOrigin];
   [request setSource:mapItemOrigin];
-  MKPlacemark *placemarkDestination = [[MKPlacemark alloc] initWithCoordinate:destination];
+  MKPlacemark *placemarkDestination = nil;
+  if ([MKPlacemark respondsToSelector:@selector(initWithCoordinate:)]) {
+    placemarkDestination = [[MKPlacemark alloc] initWithCoordinate:destination];
+  } else {
+    placemarkDestination = [[MKPlacemark alloc] initWithCoordinate:destination addressDictionary:nil];
+  }
   MKMapItem *mapItemDestination = [[MKMapItem alloc] initWithPlacemark:placemarkDestination];
   [request setDestination:mapItemDestination];
   [request setTransportType:MKDirectionsTransportTypeAny]; // This can be limited to automobile and walking directions.
--- lib/ios/AirMaps/AIRMapManager.m
+++ lib/ios/AirMaps/AIRMapManager.m
@@ -1103,26 +1103,54 @@ RCT_REMAP_METHOD(getDirections,
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
   MKDirectionsRequest *request = [[MKDirectionsRequest alloc] init];
-  MKPlacemark *placemarkOrigin = [[MKPlacemark alloc] initWithCoordinate:origin];
+  MKPlacemark *placemarkOrigin = nil;
+  // NOTE: iOS 10 v iOS 3-9 deprecated function
+  //   https://stackoverflow.com/questions/41486370/crash-on-ios-9-only-mkplacemark-initwithcoordinate-unrecognized-selector
+  if ([MKPlacemark respondsToSelector:@selector(initWithCoordinate:)]) {
+    placemarkOrigin = [[MKPlacemark alloc] initWithCoordinate:origin];
+  } else {
+    placemarkOrigin = [[MKPlacemark alloc] initWithCoordinate:origin addressDictionary:nil];
+  }
   MKMapItem *mapItemOrigin = [[MKMapItem alloc] initWithPlacemark:placemarkOrigin];
   [request setSource:mapItemOrigin];
-  MKPlacemark *placemarkDestination = [[MKPlacemark alloc] initWithCoordinate:destination];
+  MKPlacemark *placemarkDestination = nil;
+  if ([MKPlacemark respondsToSelector:@selector(initWithCoordinate:)]) {
+    placemarkDestination = [[MKPlacemark alloc] initWithCoordinate:destination];
+  } else {
+    placemarkDestination = [[MKPlacemark alloc] initWithCoordinate:destination addressDictionary:nil];
+  }
   MKMapItem *mapItemDestination = [[MKMapItem alloc] initWithPlacemark:placemarkDestination];
   [request setDestination:mapItemDestination];
   [request setTransportType:MKDirectionsTransportTypeAny]; // This can be limited to automobile and walking directions.
   [request setRequestsAlternateRoutes:YES]; // Gives you several route options.
   MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
   [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
-      if (error) {
-        reject(@"no_directions", @"There were no directions", error);
-      } else {
-        for (MKRoute *route in [response routes]) {
-          resolve([route polyline]);
-          break;
-          // [myMapView addOverlay:[route polyline] level:MKOverlayLevelAboveRoads]; // Draws the route above roads, but below labels.
-          // You can also get turn-by-turn steps, distance, advisory notices, ETA, etc by accessing various route properties.
+    if (error) {
+      reject(@"no_directions", @"There were no directions", error);
+    } else {
+      NSMutableArray* routes = [[NSMutableArray alloc] init];
+      for (MKRoute *route in [response routes]) {
+        // route is the MKRoute in this example
+        // but the polyline can be any MKPolyline
+        NSUInteger pointCount = route.polyline.pointCount;
+        // allocate a C array to hold this many points/coordinates...
+        CLLocationCoordinate2D *routeCoordinates
+        = malloc(pointCount * sizeof(CLLocationCoordinate2D));
+        // get the coordinates (all of them)...
+        [route.polyline getCoordinates:routeCoordinates
+                                 range:NSMakeRange(0, pointCount)];
+        // this part just shows how to use the results...
+        // NSLog(@"route pointCount = %d", pointCount);
+        NSMutableArray* points = [[NSMutableArray alloc] init];
+        for (int c = 0; c < pointCount; c++)
+        {
+          // NSLog(@"routeCoordinates[%d] = %f, %f", c, routeCoordinates[c].latitude, routeCoordinates[c].longitude);
+          [points addObject:@[@(routeCoordinates[c].latitude), @(routeCoordinates[c].longitude)]];
         }
+        [routes addObject:@{@"overview_polyline": @{@"points": points}}];
       }
+      resolve(routes);
+    }
   }];
 }
 
